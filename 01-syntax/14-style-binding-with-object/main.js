ee4a122edd77efee9df69ae9d5aabb683faaf612
Pryann <pryann@gmail.com>
2023-01-10 15:21:14 +0100

    Style binding with objects

---
+++ 01-syntax/14-style-binding-with-object/main.js
@@ -0,0 +1,54 @@
+const app = Vue.createApp({
+  setup() {
+    const guitars = [
+      {
+        id: 1,
+        name: 'Ibanez S6570SK-STB Prestige',
+        stock: 0,
+      },
+      {
+        id: 2,
+        name: 'Fender AM Ultra Luxe Strat MN PRB',
+        stock: 2,
+      },
+      {
+        id: 3,
+        name: 'FGN Expert Odyssey SBB',
+        stock: 11,
+      },
+      {
+        id: 4,
+        name: 'Gibson SG Standard Reissue Cherry VOS',
+        stock: 1,
+      },
+      {
+        id: 5,
+        name: 'Jackson SL2A MAH Pro Soloist UWH',
+        stock: 25,
+      },
+    ]
+
+    const outOfStock = {
+      color: 'red',
+      fontSize: '1.25rem',
+    }
+
+    const onlyFewLeftInStock = {
+      color: 'orange',
+      fontSize: '1.5rem',
+    }
+    const inStock = {
+      color: 'green',
+      fontSize: '1rem',
+    }
+
+    return {
+      guitars,
+      outOfStock,
+      onlyFewLeftInStock,
+      inStock,
+    }
+  },
+})
+
+app.mount('#app')
