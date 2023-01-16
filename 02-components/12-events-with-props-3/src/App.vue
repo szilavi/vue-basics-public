0ae5e869208c17d802bba4136c6175a140baf779
Pryann <pryann@gmail.com>
2023-01-16 12:17:10 +0100

    events-with-props-3

---
+++ 02-components/12-events-with-props-3/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { reactive } from 'vue'
+import InputField from './components/InputField.vue'
+
+const formData = reactive({
+  email: '',
+  firstName: '',
+  lastName: '',
+})
+
+function submitForm() {
+  console.log(JSON.stringify(formData))
+}
+</script>
+
+<template>
+  <InputField
+    id="email"
+    v-model="formData.email"
+    name="email"
+    type="email"
+    label="E-mail address"
+  />
+  <InputField
+    id="firstname"
+    v-model="formData.firstName"
+    name="firstname"
+    type="text"
+    label="Firstname"
+  />
+  <InputField
+    id="lastname"
+    v-model="formData.lastName"
+    name="lastname"
+    type="text"
+    label="Lastname"
+  />
+  <button @click="submitForm">Submit</button>
+</template>
+
+<style scoped></style>
---
+++ 02-components/12-events-with-props-3/src/components/InputField.vue
@@ -0,0 +1,51 @@
+<script setup>
+defineProps({
+  modelValue: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  id: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+})
+
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <div>
+    <label :for="id">{{ label }}</label>
+    <input
+      :id="id"
+      :value="modelValue"
+      :type="type"
+      :name="name"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
+
+<style scoped>
+div {
+  margin: 0 0 1rem 0;
+}
+
+label {
+  display: block;
+  font-weight: bold;
+  margin: 0 0 0.5rem 0;
+}
+</style>
