c49a1880094d3206ea2a263261c32e75d59279d2
Pryann <pryann@gmail.com>
2023-01-17 14:51:04 +0100

    list users

---
+++ 03-projects/02-user-manager/03-list-users/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { getUsers } from '../apis/usersAPI.js'
+
+const users = ref([])
+
+onMounted(async () => {
+  users.value = await getUsers()
+})
+</script>
+
+<template>
+  <table class="table table-striped table-fixed">
+    <thead>
+      <tr>
+        <th>Email Address</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>&nbsp;</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="user in users" :key="user.id">
+        <td>{{ user.emailAddress }}</td>
+        <td>{{ user.firstName }}</td>
+        <td>{{ user.lastName }}</td>
+        <td>
+          <button class="btn btn-primary me-2">Edit</button>
+          <button class="btn btn-danger">Delete</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped></style>
