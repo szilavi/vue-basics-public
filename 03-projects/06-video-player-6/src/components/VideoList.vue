cb951a5773421d1a5b95666f762f2eaa9e18dac2
Pryann <pryann@gmail.com>
2023-01-17 11:24:38 +0100

    selectVideo

---
+++ 03-projects/06-video-player-6/src/components/VideoList.vue
@@ -0,0 +1,23 @@
+<script setup>
+import VideoItem from './VideoItem.vue'
+
+defineProps({
+  videos: {
+    type: Array,
+    required: true,
+  },
+})
+
+defineEmits(['select-video'])
+</script>
+
+<template>
+  <VideoItem
+    v-for="(video, index) in videos"
+    :key="index"
+    :video="video"
+    @click="$emit('select-video', video)"
+  />
+</template>
+
+<style scoped></style>
