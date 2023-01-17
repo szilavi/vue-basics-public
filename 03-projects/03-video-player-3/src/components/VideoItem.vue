53efd31061b9a9b7fdc1ce4f7b544436af1830b5
Pryann <pryann@gmail.com>
2023-01-17 10:24:01 +0100

    VideoList and VideoItem

---
+++ 03-projects/03-video-player-3/src/components/VideoItem.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  video: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div>
+    <h2 class="h4">{{ video.snippet.title }}</h2>
+    <img
+      :src="video.snippet.thumbnails.medium.url"
+      :alt="video.snippet.title"
+    />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ 03-projects/03-video-player-3/src/components/VideoList.vue
@@ -0,0 +1,16 @@
+<script setup>
+import VideoItem from './VideoItem.vue'
+
+defineProps({
+  videos: {
+    type: Array,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <VideoItem v-for="video in videos" :key="video.id.videoId" :video="video" />
+</template>
+
+<style scoped></style>
