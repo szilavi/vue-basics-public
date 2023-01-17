5bbe8fc6bb8d679f2f438285cac316bdddcc2316
Pryann <pryann@gmail.com>
2023-01-17 11:13:30 +0100

    VideoDetails

---
+++ 03-projects/05-video-player-5/src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import { youtubeAPI, defaultParams } from './apis/youtubeAPI.js'
+import VideoList from './components/VideoList.vue'
+import VideoDetails from './components/VideoDetails.vue'
+
+const videos = ref(null)
+const selectedVideo = ref(null)
+
+async function searhVideos(query) {
+  const response = await youtubeAPI.get('/search', {
+    params: {
+      ...defaultParams,
+      q: query,
+    },
+  })
+  videos.value = response.data.items
+  selectedVideo.value = videos.value[0]
+}
+
+onMounted(() => searhVideos('vuejs'))
+</script>
+
+<template>
+  <header>
+    <div class="container">SearchBar</div>
+  </header>
+  <main>
+    <div class="container">
+      <div v-if="videos" class="row">
+        <div class="col-lg-8">
+          <VideoDetails :video="selectedVideo" />
+        </div>
+        <div class="col-lg-4">
+          <VideoList :videos="videos" />
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
---
+++ 03-projects/05-video-player-5/src/components/VideoList.vue
@@ -0,0 +1,16 @@
+<script setup>
+import VideoItem from './VideoItem.vue'
+
+defineProps({
+  videos: {
+    type: Array,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <VideoItem v-for="(video, index) in videos" :key="index" :video="video" />
+</template>
+
+<style scoped></style>
