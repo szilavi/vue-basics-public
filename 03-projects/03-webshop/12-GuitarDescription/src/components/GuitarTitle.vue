42604fbb6122965f3e8e1c4ae258dca34988c823
Pryann <pryann@gmail.com>
2023-01-20 15:10:55 +0100

    GuitarDescription

---
+++ 03-projects/03-webshop/12-GuitarDescription/src/components/GuitarTitle.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <h2 class="guitar__title">{{ title }}</h2>
+</template>
+
+<style scoped>
+.guitar__title {
+  margin: 0 0 0.5rem 0;
+}
+</style>
