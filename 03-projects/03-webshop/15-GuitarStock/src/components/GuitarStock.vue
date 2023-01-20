39eda3816a3a4873efef2b747b96a1103381bf46
Pryann <pryann@gmail.com>
2023-01-20 15:56:13 +0100

    GuitarStock

---
+++ 03-projects/03-webshop/15-GuitarStock/src/components/GuitarStock.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  stock: {
+    type: Number,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="guitar__stock">
+    <span v-if="stock === 0" class="text-red">Out of stock</span>
+    <span v-else-if="stock < 3" class="text-orange">
+      Last chance, only {{ stock }} left
+    </span>
+    <span v-else class="text-green">In stock</span>
+  </div>
+</template>
+
+<style scoped>
+.guitar__stock {
+  font-weight: bold;
+}
+</style>
