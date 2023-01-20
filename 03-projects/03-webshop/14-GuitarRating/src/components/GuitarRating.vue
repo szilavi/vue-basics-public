9530011968292ba8497cfe79ca66ffcbb90fbac0
Pryann <pryann@gmail.com>
2023-01-20 15:34:18 +0100

    GuitarRating

---
+++ 03-projects/03-webshop/14-GuitarRating/src/components/GuitarRating.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  rating: {
+    type: Number,
+    required: true,
+  },
+})
+
+const ratingStars = computed(
+  () => `
+  ${'★'.repeat(props.rating)}
+  ${'☆'.repeat(5 - props.rating)}
+  (${props.rating})
+`
+)
+</script>
+
+<template>
+  <div class="guitar__rating">
+    <span v-if="rating > 0">{{ ratingStars }}</span>
+    <span v-else>Not rated</span>
+  </div>
+</template>
+
+<style scoped></style>
