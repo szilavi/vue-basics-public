aa2c1da7b3e7adb1bddb04e2fbb4d134ca634fb8
Pryann <pryann@gmail.com>
2023-01-23 09:49:08 +0100

    CartButton-2

---
+++ 03-projects/03-webshop/17-CartButton-2/src/components/CartButton.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  guitar: {
+    type: Object,
+    required: true,
+  },
+})
+
+const count = ref(0)
+
+function handleDescreaseCountClick() {
+  count.value -= 1
+}
+
+function handleIncreaseCountClick() {
+  count.value += 1
+}
+function handleCartButtonClick() {}
+</script>
+
+<template>
+  <div class="cart">
+    <button @click="handleDescreaseCountClick">-</button>
+    <input v-model="count" type="text" />
+    <button @click="handleIncreaseCountClick">+</button>
+    <button class="add-to-cart" @click="handleCartButtonClick">
+      Add to cart
+    </button>
+  </div>
+</template>
+
+<style scoped>
+button {
+  padding: 0.25rem 1rem;
+  background: var(--dark);
+  border: 1px solid var(--dark);
+  color: var(--light);
+}
+
+button:active {
+  background: var(--light);
+  color: var(--dark);
+}
+
+buttom:hover {
+  cursor: pointer;
+}
+
+input {
+  border: 1px solid var(--dark);
+  padding: 0.25rem;
+  border-radius: 0;
+  width: 5ch;
+  text-align: center;
+}
+
+.add-to-cart {
+  margin: 0 0 0 1rem;
+}
+</style>
---
+++ 03-projects/03-webshop/17-CartButton-2/src/components/GuitarItem.vue
@@ -0,0 +1,45 @@
+<script setup>
+import CartButton from './CartButton.vue'
+import GuitarDescription from './GuitarDescription.vue'
+import GuitarImage from './GuitarImage.vue'
+import GuitarPrice from './GuitarPrice.vue'
+import GuitarRating from './GuitarRating.vue'
+import GuitarStock from './GuitarStock.vue'
+import GuitarTitle from './GuitarTitle.vue'
+
+defineProps({
+  guitar: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="guitar">
+    <div class="left-column">
+      <GuitarImage :title="guitar.title" />
+    </div>
+    <div class="right-column">
+      <GuitarTitle :title="guitar.title" />
+      <GuitarDescription :description="guitar.description" />
+      <GuitarPrice :price="guitar.price" />
+      <GuitarRating :rating="guitar.rating" />
+      <GuitarStock :stock="guitar.stock" />
+      <CartButton />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.guitar {
+  box-shadow: 0.25rem 0.1rem 0.5rem rgba(0, 0, 0, 0.15);
+  display: flex;
+  padding: 1rem;
+  margin: 0 0 2rem 0;
+}
+
+.right-column {
+  padding: 0 0 0 3rem;
+}
+</style>
